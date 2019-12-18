92ce29c2207b4be12431618478eb44a5feed9933
nvrsantos <playcar46@gmail.com>
2019-12-18 16:23:32 -0300

    Web Documentation

--- EasyMenu/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-from EasyMenu import (
-    menu as menu,
-    menu_nocolor as menu_nocolor
-)
--- example.py
+++
@@ -1,8 +0,0 @@
-from EasyMenu import *
-option = {
-    "Sim": "print('Hello, World!')",
-    "Não": "exit()"
-}
-
-menu = menu_nocolor.Menu(option, 'Deseja continuar ?', 'Escolha uma opção: ')
-eval(menu.display())
